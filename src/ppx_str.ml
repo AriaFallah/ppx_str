1f02ab1d7c543c7f267b81e915b3b63d82d115ad
AriaFallah <aria@fallah.us>
2018-09-23 23:10:05 -0700

    first commit

---
+++ src/ppx_str.ml
@@ -0,0 +1,28 @@
+open Ppxlib
+
+let name = "str"
+
+let expand ~loc ~path:_ str =
+  let open Ast_builder.Default in
+  let open Re.Str in 
+  let strs = str
+    |> full_split (regexp {|{\w+}|})
+    |> List.map (fun r -> 
+      match r with 
+        | Text s  -> estring ~loc s
+        | Delim s ->
+          let start = 1 in
+          let len = String.length s in
+          evar ~loc (String.sub s start (len - 1 - start))
+    ) in
+  [%expr String.concat "" [%e elist ~loc strs]]
+
+let ext =
+  Extension.declare
+    name
+    Extension.Context.expression
+    Ast_pattern.(single_expr_payload (estring __))
+    expand
+
+let () =
+  Ppxlib.Driver.register_transformation name ~extensions:[ext]
---
+++ test/test.ml
@@ -0,0 +1,4 @@
+let name = "aria"
+let name2 = "ppx_str"
+
+let my_str = [%str {|Hello there {name} I am {name2}|}]
